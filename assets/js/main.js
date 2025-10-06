14996693089646dc25e4cabdb05f7620df77bfaa
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-10-06 17:40:54 +0200

    sistemato codice e aggiunto parte per per 2 decimali

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,7 @@ console.log(younger_age);
 const over_age = 65
 console.log(over_age);
 
-let ticket_cost = 0.21 * km
-console.log(ticket_cost);
+
 
 
 // finestra per inserire età
@@ -23,14 +22,25 @@ console.log(age);
 const km = Number(prompt('inserisci chilometri da percorrere'))
 console.log(km);
 
+// calcolo prezzo intero
+let ticket_cost = 0.21 * km
+console.log(ticket_cost);
 
+//  decidere se applicare lo sconto
 if (age < younger_age) {
     // sconto d'applicare agli under 18
     ticket_cost = ticket_cost - ticket_cost * 20 / 100
+    
 } else if (age > over_age) {
     // sconto d'applicare agli over 65
     ticket_cost = ticket_cost - ticket_cost * 40 / 100 
 } 
+
+console.log(ticket_cost);
+
+let prezzoFormattato = ticket_cost.toFixed(2);
+console.log(prezzoFormattato);
+
 
 
 
